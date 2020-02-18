02f28aab8e21613509348011230ff842a03d9294
Luiz Carlos <Luiz Carlos@DESKTOP-GM3JE6P>
2020-02-18 12:38:08 -0300

    funcionalidades de cadastrar e listar todos funcionando

--- papelariaWeb/js/app.js
+++ papelariaWeb/js/app.js
@@ -1,70 +1,54 @@
 (function () {
-    'use strict';
+	'use strict';
 
-    angular.module('papelariaWeb', [])
-        .controller('ProdutoController', function ($scope,$http) {
-        	
-        	
-        	   $scope.cadastrarProdutoNovo = function () {
-        		   $http.post('http://localhost:9000/papelaria/produtos/novo',
-               			$scope.produto );
-                   	    
-                   	};
-        	
-                   	
-        	
-        	//método para adicionar o produto a lista
-            $scope.processar = function () {
-            	
-            	//if(event.target[7].id==){
-            		
-            	//$scope.cadastrarProdutoNovo();
-                //$scope.produtostodos();
-            	
-            	
-            	//alert(event.explicitOriginalTarget.id);
-            	
-            	if(event.explicitOriginalTarget.id=='salvar'){
-            		$scope.cadastrarProdutoNovo();
-                    $scope.produtostodos();
-            	}
-            	if(event.explicitOriginalTarget.id=='editar'){
-            		alert('editar');
-            	}
-            	if(event.explicitOriginalTarget.id=='excluir'){
-            		alert('excluir');
-            	}
-            	
-               
-            	
-            		
-            	
-            	
-            };
-            
-             $scope.produtostodos = function () {
-            	$http.get('http://localhost:9000/papelaria/produtos/todos').then(function (response) {
-                    $scope.produtos = {id: '', nome: '', categoria: '', detalhe: '', codBarras: '',descricao:'',precoMedio:'',quantidade:''};
-                    $scope.produtos=response.data;
-            	});
-            };
-            
-         
-            
-            
-         
-           
-            
-            
-            
-            
-                        
-          
-          
-            
-            
-            
-        });
+	angular.module('papelariaWeb', [])
+	.controller('ProdutoController', function ($scope,$http) {
+
+
+		$scope.cadastrarProdutoNovo = function () {
+			$http.post('http://localhost:9000/papelaria/produtos/novo',$scope.produto );
+		};
+
+
+
+		//método para adicionar o produto a lista
+		$scope.processar = function () {
+
+			if(event.explicitOriginalTarget.id=='salvar'){
+				$scope.cadastrarProdutoNovo();
+				$scope.produtostodos();
+			}
+			if(event.explicitOriginalTarget.id=='editar'){
+				alert('editar');
+			}
+			if(event.explicitOriginalTarget.id=='excluir'){
+				alert('excluir');
+			}
+		};
+
+		$scope.produtostodos = function () {
+			$http.get('http://localhost:9000/papelaria/produtos/todos').then(function (response) {
+				$scope.produtos = {id: '', nome: '', categoria: '', detalhe: '', codBarras: '',descricao:'',precoMedio:'',quantidade:''};
+				$scope.produtos=response.data;
+			});
+		};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	});
 
 })();
 
