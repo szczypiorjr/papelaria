07bce801f0c828c1b4e156c4031534295756459b
zzpio <zzpio@awakener>
2025-07-08 14:35:26 -0300

    permitindo pra testes todas as origens de requisições

--- papelaria/src/main/java/br/org/szczypiorjr/papelaria/application/controller/CategoriaController.java
+++ papelaria/src/main/java/br/org/szczypiorjr/papelaria/application/controller/CategoriaController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,7 +21,8 @@ import br.org.szczypiorjr.papelaria.application.dto.CategoriaDTO;
 import br.org.szczypiorjr.papelaria.application.service.CategoriaService;
 import jakarta.validation.Valid;
 
-
+//@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/categoria")
 public class CategoriaController
